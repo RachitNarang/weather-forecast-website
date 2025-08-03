9b4f1055f828845c01a95c3050f0621ed63edeba
Rachit Narang <rachitnarang27@gmail.com>
2025-08-03 12:55:39 +0530

    rachit

--- jav.js
+++ jav.js
@@ -4,10 +4,10 @@ const search = document.querySelector("#search")
 const weather = document.querySelector("#weather")
 const getWeather = async(city) => {
     weather.innerHTML = `<h2>  Loading...  <h2>`
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url);
     const data = await response.json()
-    return showWeather(data)
+     return showWeather(data)
 }
 
 const showWeather = (data) => {
